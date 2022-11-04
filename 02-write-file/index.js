9ddb1d7af7e9bb0ce6f300255bb893a1678f3e39
Andrey Maximov <c0za@mail.ru>
2022-11-04 20:00:47 +0300

    feat: solve 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const readline = require('readline')
+const rl = readline.createInterface(stdin, stdout);
+const file = path.join(__dirname, 'text.txt');
+
+const writeStream = fs.createWriteStream(file, 'utf8');
+
+stdout.write('Введите текстовое сообщение: \n');
+rl.on('line', data => {
+  if (data === 'exit') {
+    process.exit();
+  }
+  writeStream.write(`${data} \n`);
+});
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => stdout.write('Увидимся, Student 1..))'));
